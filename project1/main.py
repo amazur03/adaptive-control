eeed238f1d6c714b6b09cb4ddabfe2db31b322e1
Adam Mazur <adam.mazur.mailbox@gmail.com>
2024-10-24 13:56:48 +0200

    variance update

--- project1/main.py
+++ project1/main.py
@@ -1,8 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Define the time range from 0 to 2π
-t = np.linspace(0, 2 * np.pi, 300)
+# Define the time range from 0 to 6π
+t = np.linspace(0, 6 * np.pi, 1000)
 
 # Signal frequency
 f = 2 / (2 * np.pi)
@@ -12,12 +12,12 @@ signal = 2 * np.abs(2 * (t * f - np.floor(t * f + 0.5))) - 1
 
 noise = []
 denoised = []
-d = 0.25  # Amplitude of the noise
-H = 10    # Horizon (number of previous samples to average)
+d = 0.1  # Variance of the noise
+H = 5    # Horizon (number of previous samples to average)
 
 # Adding noise to the signal
 for i in range(len(t)):
-    noise.append(signal[i] + d * np.random.randn())
+    noise.append(signal[i] + (np.sqrt(d) * np.random.randn()))
 
 # Calculate the moving average with a horizon of H
 for i in range(len(t)):
@@ -30,15 +30,15 @@ for i in range(len(t)):
 
 # Plot the original signal, noisy signal, and denoised signal
 plt.figure(figsize=(8, 4))
-plt.plot(t, signal, label='Original Signal', color='blue')
+plt.scatter(t, signal, label='Original Signal', color='blue', s = 4)
 
 # Scatter plot of the noisy signal in red with smaller dots
-plt.scatter(t, noise, color='red', label='Noisy Signal', s=10)
+plt.scatter(t, noise, color='red', label='Noisy Signal', s = 4)
 
 # Plot the denoised signal in green
 plt.plot(t, denoised, label='Denoised Signal', color='green')
 
-plt.title("Triangular Signal with Noise and Denoised Signal (Moving Average)")
+plt.title("Signal")
 plt.xlabel("Time [s]")
 plt.ylabel("Amplitude")
 plt.grid(True)
