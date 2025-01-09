a7d07b33f3a2644195fffad67be545428389d67e
Adam Mazur <adam.mazur.mailbox@gmail.com>
2025-01-10 00:50:07 +0100

    P, LQR

--- project3/part1.m
+++ project3/part1.m
@@ -30,8 +30,8 @@ tspan = [0 10];
 % Rozwiązywanie równań różniczkowych za pomocą ode45
 [t, x] = ode45(odefun, tspan, x0);
 
-% Obliczanie odpowiedzi wyjściowej (y = Cx + Du)
-y = Cc * x' + Dc * u;
+% Obliczanie odpowiedzi wyjściowej (y = Cx)
+y = Cc * x';
 
 % Rysowanie wykresu odpowiedzi skokowej
 figure;
@@ -43,8 +43,59 @@ grid on;
 
 % ---------- Regulacja Proporcjonalna ----------
 y_z = 1;  % Wartość zadana
-epsilon = y_z - y;  % Uchyb (różnica między wartością zadaną a rzeczywistą)
 kp = 1;  % Wzmocnienie regulatora proporcjonalnego
 
-% Regulacja (obliczanie nowego wejścia na podstawie uchybu)
-u = kp * epsilon;
+% Rozwiązywanie ODE z regulacją proporcjonalną
+odefun_regulator = @(t, x) Ac * x + Bc * (kp * (y_z - Cc * x));  % Równanie stanu z regulatorem
+[t, x2] = ode45(odefun_regulator, tspan, x0);
+
+% Obliczanie odpowiedzi wyjściowej (y = Cx)
+y2 = Cc * x2';
+
+% Rysowanie wykresu odpowiedzi układu regulowanego
+figure;
+plot(t, y2);
+xlabel('Czas (s)');
+ylabel('Odpowiedź układu regulowanego y(t)');
+title('Odpowiedź układu z regulatorem P');
+grid on;
+
+% ---------- Regulacja LQR z wartością zadaną ----------
+% Parametry regulacji
+Q = eye(2);  % Penalizacja stanów
+R = 0.1;     % Penalizacja sterowania
+
+% Obliczenie macierzy wzmocnienia LQR
+K = lqr(Ac, Bc, Q, R);
+
+% Wartość zadana wyjścia
+y_z = 1;
+
+% Obliczenie x_ref i u_ref
+x_ref = pinv(Cc) * y_z;  % Wartość odniesienia stanu
+u_ref = -pinv(Bc) * (Ac * x_ref);  % Wartość odniesienia sterowania
+
+% Równanie różniczkowe z regulacją LQR
+odefun_lqr = @(t, x) Ac * x + Bc * (u_ref - K * (x - x_ref));
+
+% Początkowe warunki
+x0 = [0; 0];  % Stan początkowy
+
+% Zakres czasu
+tspan = [0 10];
+
+% Rozwiązanie równań różniczkowych
+[t, x_lqr] = ode45(odefun_lqr, tspan, x0);
+
+
+
+% Obliczanie odpowiedzi wyjściowej
+y_lqr = Cc * x_lqr';
+
+% Rysowanie wykresu odpowiedzi
+figure;
+plot(t, y_lqr);
+xlabel('Czas (s)');
+ylabel('Odpowiedź wyjściowa y(t)');
+title('Odpowiedź układu z regulatorem LQR');
+grid on;
